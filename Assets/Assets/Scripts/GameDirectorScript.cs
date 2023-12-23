97d91823ddc44b48772135ffdd63d4ebdc6af31f
Raphael Nogueira Rezende Laroca Pinto <raphael.laroca@gmail.com>
2023-12-22 21:02:12 -0300

    Added build

--- Assets/Assets/Scripts/GameDirectorScript.cs
+++ Assets/Assets/Scripts/GameDirectorScript.cs
@@ -2,8 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEditor.UIElements;
-using UnityEngine.UI;
 using TMPro;
 
 public class GameDirectorScript : MonoBehaviour
